7f757eb9249e048d0d13f6435d4d01d13799f23e
裴光伟 <hrms2019@163.com>
2019-10-20 10:30:57 +0800

    Initial commit

---
+++ src/car/Brush.java
@@ -0,0 +1,21 @@
+package car;
+
+public class Brush {
+	private int speed;
+
+	public Brush() {
+
+	}
+
+	public Brush(int speed) {
+		this.speed = speed;
+	}
+
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+}
---
+++ src/car/Lever.java
@@ -0,0 +1,30 @@
+package car;
+
+public class Lever {
+	private int pos;
+
+	public Lever() {
+
+	}
+
+	public Lever(int pos) {
+		this.pos = pos;
+	}
+
+	public int getPos() {
+		return pos;
+	}
+
+	public void upPos() {
+		if (this.pos < 4) {
+			this.pos++;
+		}
+	}
+
+	public void downPos() {
+		if (this.pos > 1) {
+			this.pos--;
+		}
+	}
+
+}
